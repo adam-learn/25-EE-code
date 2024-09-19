b6c4a69336080eb2ebf0cf6eb2e9f70a307107c5
RYJ <3529742497@qq.com>
2024-09-19 13:33:28 +0800

    DAM删除

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -106,7 +106,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
- MX_DMA_Init();
+ // MX_DMA_Init();
   MX_FDCAN1_Init();
   MX_FDCAN2_Init();
   MX_FDCAN3_Init();
